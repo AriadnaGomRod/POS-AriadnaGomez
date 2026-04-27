cf2f388992239a04e4bb768b0b82ba4f50243715
AriadnaGomRod <arigoro04@gmail.com>
2026-04-27 00:26:27 -0600

    Creación de la clase Empleado

---
+++ src/Modelo/Empleado.java
@@ -0,0 +1,89 @@
+package Modelo;
+
+/**
+ *
+ * @author arigo
+ */
+/**
+ * Clase para manejar los empleados del sistema.
+ */
+public class Empleado {
+    //Creación de las variables
+    private int idEmpleado;
+    private String nombre;
+    private String aPaterno;
+    private String aMaterno;
+    private String telefono;
+    private String usuario;
+    private String contrasena;
+    private String rol;
+
+    //Constructor vacío
+    public Empleado() {}
+
+    //Getters y setters
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getaPaterno() {
+        return aPaterno;
+    }
+
+    public void setaPaterno(String aPaterno) {
+        this.aPaterno = aPaterno;
+    }
+
+    public String getaMaterno() {
+        return aMaterno;
+    }
+
+    public void setaMaterno(String aMaterno) {
+        this.aMaterno = aMaterno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+}
