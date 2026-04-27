0c72baac97daca5ce957e406c5fb5a532620a2a5
AriadnaGomRod <arigoro04@gmail.com>
2026-04-27 00:28:19 -0600

    Creación de la clase Producto

---
+++ src/Modelo/Producto.java
@@ -0,0 +1,90 @@
+package Modelo;
+
+/**
+ *
+ * @author arigo
+ */
+/**
+ * Clase para manejar los productos.
+ */
+public class Producto {
+    //Creación de las variables
+    private int idProducto;
+    private String codigoBarras;
+    private String nomProd;
+    private double precio;
+    private int stock;
+    private int stockMin;
+    private int idCategoria;
+    private int idProveedor;
+
+    //Constructor
+    public Producto() {}
+
+
+    //Getters y setters
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public String getNomProd() {
+        return nomProd;
+    }
+
+    public void setNomProd(String nomProd) {
+        this.nomProd = nomProd;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getStockMin() {
+        return stockMin;
+    }
+
+    public void setStockMin(int stockMin) {
+        this.stockMin = stockMin;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+    
+}
