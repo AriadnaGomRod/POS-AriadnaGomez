b0b3d51e273e1bb5692b015dd02afd29c3ac5b39
AriadnaGomRod <arigoro04@gmail.com>
2026-04-27 00:24:01 -0600

    Se creó la clase DetalleVenta

---
+++ src/Modelo/DetalleVenta.java
@@ -0,0 +1,63 @@
+package Modelo;
+
+/**
+ *
+ * @author arigo
+ */
+/**
+ * Clase para manejar el detalle de una venta.
+ */
+public class DetalleVenta {
+    //Creación de las variables
+    private int idVenta;
+    private int idProducto;
+    private int cantidad;
+    private double precioUnitario;
+    private double subtotal;
+
+    //Constructor vacío
+    public DetalleVenta() {}
+
+
+    //Getters y setters
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+    
+}
